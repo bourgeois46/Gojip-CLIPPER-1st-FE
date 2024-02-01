1eea390f32d60901d3605c73ad59f13d51add447
이채원 <cindy@ichaewon-ui-MacBookPro.local>
2024-02-01 19:35:19 +0900

    [feat] #6 기록하기-비용 charge-box UI

--- src/pages/RecordPage/RecordPage.js
+++ src/pages/RecordPage/RecordPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from '../../components/Nav/Nav';
 import './RecordPage.css';
+import record_slash from "../../assets/images/record_slash.png";
 
 function RecordPage() {
     return (
@@ -12,6 +13,35 @@ function RecordPage() {
             <div className="place-yellow">
                 서울 서대문구 신촌동 2-195
             </div>
+
+            <div className='charge-box'>
+                <div className="charge-title">비용</div>
+                <div className='charge-item-row'>
+                    <div className="charge-details">
+                        <text className="charge-label-deposit">보증금 / 월세</text>
+                        <text className="charge-label-monthly-rent">관리비</text>
+                    </div>
+                    <div className='line-charge'></div>
+                    <div className="charge-details">
+                        <div className="charge-item-up">
+                            <input type="text" className="deposit-input" placeholder="입력" />
+                            <img className='slash' alt='record_slash' src={record_slash}/>
+                            <input type="text" className="monthly-rent-input" placeholder="입력" />
+                            <span className="charge-unit">원</span>
+                        </div>
+                        <div className="charge-item-down">
+                            <label className="charge-option-yes">
+                                <input type="checkbox" className="charge-yes" value="있다" /> 있다
+                            </label>
+                            <label className="charge-option-no">
+                                <input type="checkbox" className="charge-no" value="없다" /> 없다
+                            </label>
+            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
