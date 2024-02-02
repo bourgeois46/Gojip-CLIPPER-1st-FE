466bfe7fe4b79c52f6add9bf3d15c772dd9ac769
bourgeois <juahsome2795@gmail.com>
2024-02-02 15:26:05 +0900

    feat: 네비게이션 구현

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Nav.css";
+import homeyellow from "../../assets/images/homeYellow.png";
+import sidebarbutton from "../../assets/images/sidebarButton.png";
+
+const Nav = () => {
+  return (
+    <div className="nav-container">
+      <div className="go-home">Go집</div>
+      <img src={homeyellow} alt="HomeYellow" className="homeyellow-icon" />
+      <img
+        src={sidebarbutton}
+        alt="SidebarButton"
+        className="sidebarbutton-icon"
+      />
+    </div>
+  );
+};
+
+export default Nav;
