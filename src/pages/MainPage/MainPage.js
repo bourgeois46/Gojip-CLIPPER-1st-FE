a9069bb69e0e2942e330190b93ec8dda42193ae6
bourgeois <juahsome2795@gmail.com>
2024-02-13 16:45:15 +0900

    feat: 애니메이션 구현

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -11,13 +11,13 @@ import main7 from "../../assets/images/main-7.png";
 const MainPage = () => {
   return (
     <div className="main-page">
-      <img src={main1} alt="main-1" className="main1 image-animation" />
-      <img src={main2} alt="main-2" className="main2 image-animation" />
-      <img src={main3} alt="main-3" className="main3 image-animation" />
-      <img src={main4} alt="main-4" className="main4 image-animation" />
-      <img src={main5} alt="main-5" className="main5 image-animation" />
-      <img src={main6} alt="main-6" className="main6 image-animation" />
-      <img src={main7} alt="main-7" className="main7 image-animation" />
+      <img src={main1} alt="main-1" className="main1" />
+      <img src={main2} alt="main-2" className="main2" />
+      <img src={main3} alt="main-3" className="main3" />
+      <img src={main4} alt="main-4" className="main4" />
+      <img src={main5} alt="main-5" className="main5" />
+      <img src={main6} alt="main-6" className="main6" />
+      <img src={main7} alt="main-7" className="main7" />
     </div>
   );
 };
